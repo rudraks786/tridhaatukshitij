f6005d59c7085f14615e45fb39f57d222dab29ca
rudraks786 <vijay.s.tripathi@gmail.com>
2021-10-13 17:35:16 +0530

    commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// Code based up FlexSlider Video demo:
+// https://flexslider.woothemes.com/video.html
+$(document).ready(function() {
+  // Call fitVid before FlexSlider initializes, so the proper initial height can be retrieved.
+  $("#video-slider")
+	//.fitVids()
+	.flexslider({
+  	animation:"slide",
+  	useCSS:false,
+  	smoothHeight: true,
+  	slideshowSpeed: 3000,
+	video: true,  
+	  before: function(){ $("video").each( function() { $(this).get(0).load(); }); }
+  });
+});
+
+function pauseslider() { $("#video-slider").flexslider("pause"); }
+function playslider() { $("#video-slider").flexslider("play"); }
+function resumeslider() { $("#video-slider").flexslider("next"); $("#video-slider").flexslider("play"); }
